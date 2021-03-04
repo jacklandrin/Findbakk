dcc063c241cfaa6ac243bd6945a17bdfed3a9163
jacklandrin <jacklandrin@hotmail.com>
2021-03-04 21:07:49 +0100

    Initial Commit

---
+++ Findbakk/FindbakkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindbakkApp.swift
+//  Findbakk
+//
+//  Created by jack on 2021/3/4.
+//
+
+import SwiftUI
+
+@main
+struct FindbakkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
